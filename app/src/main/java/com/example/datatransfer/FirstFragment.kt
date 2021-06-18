7b4d08177d3db233131e3b92f1a13fd1f9cde96d
EomJunyoung <theoneday737@naver.com>
2021-06-18 12:31:58 +0900

    add fragment viewbinding and convert fragment1 to fragment2 using navi

--- app/src/main/java/com/example/datatransfer/FirstFragment.kt
+++ app/src/main/java/com/example/datatransfer/FirstFragment.kt
@@ -1,11 +1,17 @@
 package com.example.datatransfer
 
+import android.app.Application
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.fragment.findNavController
+import com.example.datatransfer.databinding.FragmentFirstBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,12 +25,20 @@ private const val ARG_PARAM2 = "param2"
  */
 class FirstFragment : Fragment(R.layout.fragment_first) {
 
-    private lateinit var binding: FragmentActivity
+    private lateinit var binding: FragmentFirstBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+//        binding = FragmentFirstBinding.inflate(layoutInflater)
 
+        val binding2 = FragmentFirstBinding.bind(view) // fragment에서 viewBinding 하는법
+
+        binding2.button.setOnClickListener {
+
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+
+        }
     }
 
 
--- app/src/main/java/com/example/datatransfer/SecondFragment.kt
+++ app/src/main/java/com/example/datatransfer/SecondFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.datatransfer.databinding.FragmentFirstBinding
+import com.example.datatransfer.databinding.FragmentSecondBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,4 +21,16 @@ private const val ARG_PARAM2 = "param2"
  */
 class SecondFragment : Fragment(R.layout.fragment_second) {
 
+    //onCreateView는 View를 생성하는 메소드이며, 해당 메소드에서 return한 view가
+    //onViewCreate가 받는것임(fragment)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentSecondBinding.bind(view)
+
+        binding.button2.setOnClickListener {
+            findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+
+    }
 }
